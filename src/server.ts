dd7a5e3e208ae4c498cea6ff586131d6b5fd58da
Pedro Silva <pedro_rike@hotmail.com.br>
2025-07-05 08:17:43 -0300

    chore: add eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
-app.get("/hello", async () => {
-  return { hello: "world" };
-});
+app.get('/hello', async () => {
+  return { hello: 'world' }
+})
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log("http server running");
-  });
+    console.log('http server running')
+  })
