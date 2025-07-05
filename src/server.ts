a68e4b6fb063741b3fbd865d4d2acf6d2db1b478
Pedro Silva <pedro_rike@hotmail.com.br>
2025-07-05 08:12:19 -0300

    chore: config init project

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/hello", async () => {
+  return { hello: "world" };
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("http server running");
+  });
