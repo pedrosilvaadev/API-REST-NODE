d7f43123ab652874be15f05cc899001ce37441ba
Pedro Silva <pedro_rike@hotmail.com.br>
2025-07-26 09:16:20 -0300

    config: .env

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transactions = await knex('transactions').insert({
+    id: crypto.randomUUID(),
+    title: 'New transaction',
+    amount: 5000,
+  })
+
+  return { transactions }
 })
 
 app
